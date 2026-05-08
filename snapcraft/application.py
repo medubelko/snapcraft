a76842110236a4ad24ff778eeb8102c04d09735d
Michael DuBelko <michael.dubelko@canonical.com>
2026-05-08 15:25:52 -0700

    chore: make a change

--- snapcraft/application.py
+++ snapcraft/application.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Main Snapcraft Application."""
+"""Main Snapcraft Application. !!! TEST"""
 
 from __future__ import annotations
 
